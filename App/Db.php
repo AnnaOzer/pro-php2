b460713748e7436329a3695d3e5a05594beddedb
AnnaOzer <ozerskaya.ru@mail.ru>
2016-01-28 12:16:56 +0300

    + метод execute() класса Db

--- App/Db.php
+++ App/Db.php
@@ -10,9 +10,18 @@ namespace App;
 
 
 class Db {
+
+    protected $dbh;
+
     public function __construct()
     {
-        echo 'Hello, DB!';
+        $this->dbh = new \PDO('mysql:host=127.0.0.1;dbname=pro-php2-test', 'root', '');
     }
 
-} 
+    public function execute($sql){
+        $sth = $this->dbh->prepare($sql);
+        $res = $sth->execute();
+        return $res;
+    }
+
+}
--- index.php
+++ index.php
@@ -8,3 +8,5 @@
 
 require __DIR__ . '/autoload.php';
 $db = new \App\Db();
+
+$res = $db->execute('CREATE TABLE foo(id SERIAL)');
