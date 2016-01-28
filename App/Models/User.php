d5c16a2bd35dd7d726a5704dcf23fab253af4357
AnnaOzer <ozerskaya.ru@mail.ru>
2016-01-29 00:55:09 +0300

    + ::findAll() Warning: require(C:\OpenServer\domains\pro-php2/App/Models/Db.php): failed to open stream: No such file or directory in C:\OpenServer\domains\pro-php2\autoload.php on line 11
    Warning: require(C:\OpenServer\domains\pro-php2/App/Models/Db.php): failed to open stream: No such file or directory in C:\OpenServer\domains\pro-php2\autoload.php on line 11
    
    Fatal error: require(): Failed opening required 'C:\OpenServer\domains\pro-php2/App/Models/Db.php' (include_path='.;c:/openserver/modules/php/PHP-5.6;c:/openserver/modules/php/PHP-5.6/PEAR/pear') in C:\OpenServer\domains\pro-php2\autoload.php on line 11

---
+++ App/Models/User.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Пользователь
+ * Date: 29.01.2016
+ * Time: 0:33
+ */
+
+namespace App\Models;
+
+
+class User {
+    public $email;
+    public $name;
+
+    public static function findAll()
+    {
+        $db = new Db();
+        return $db->query(
+            'SELECT * FROM users',
+            'App\Models\User'
+        );
+    }
+
+} 
