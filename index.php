3343ff345aaff2f95c8b1b7964869dbb34ac3b16
AnnaOzer <ozerskaya.ru@mail.ru>
2016-03-01 15:29:38 +0300

    + фронт-контроллер

--- index.php
+++ index.php
@@ -1,49 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Пользователь
- * Date: 28.01.2016
- * Time: 10:36
- */
 
 require __DIR__ . '/autoload.php';
-/*
-use App\Models\HasEmail;
-use App\Models\User;
-
-
-$user = new User();
-$user->name = 'Vasya';
-$user->email = 'v@pupkin.ru';
-$user->insert();
-*/
-
-
-/*$user = new \App\Models\User();
-$user->getEmail();
-*/
-
-
-/*
-$view = new \App\View();
-$view->title="Мой крутой сайт!";
-*/
-/*$view->desc = 'Сайт про новости';
-
-$view->users = \App\Models\User::findAll();
-
-
-echo count($view);
-die;
-
-echo $view->render(__DIR__ . '/App/templates/index.php');
-*/
-
-/*
-$view->news = \App\Models\News::findAll();
-
-$view->display(__DIR__ . '/App/templates/index.php');
-*/
-
 $controller = new \App\Controllers\News();
-$controller->action('Index');
+
+$action =$_GET['action'] ?: 'Index';
+
+$controller->action($action);
