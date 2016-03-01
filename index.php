f18ddd852feb96367d6b3d9ce68914c944103e2e
AnnaOzer <ozerskaya.ru@mail.ru>
2016-03-01 17:09:49 +0300

    + mod_rewrite

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
+// var_dump($_GET); die;
+// var_dump($_POST); die;
+// var_dump($_REQUEST); die;
+$url = $_SERVER['REQUEST_URI'];
+
 require __DIR__ . '/autoload.php';
 $controller = new \App\Controllers\News();
 
